7117c6293c4b98e99013637dd1ffe65813d7dfc5
SARAH <sarah@SARAHs-MacBook-Pro.local>
2021-05-24 10:51:46 +0200

    Finish tablet size design. Add javascript file to control invalid email input and display error. Start mobile size design.

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector('.register-btn').addEventListener('click', function(){
+  let valid = emailIsValid(document.querySelector('.email').value);
+
+  if(valid) {
+    document.querySelectorAll('label')[0].style.display = 'none';
+    document.querySelector('.icon-error').style.display = 'none';
+    document.querySelector('.register').style.border = 'solid 1px hsl(0, 36%, 70%)';
+  }else {
+    document.querySelectorAll('label')[0].style.display = 'inline-block';
+    document.querySelector('.icon-error').style.display = 'inline-block';
+    document.querySelector('.register').style.border = "2px solid hsl(0, 93%, 68%)";
+  }
+});
+
+
+function  emailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
