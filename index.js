382b6120f2348c1961f33093f0cbe6f2edd0ba7b
SARAH <sarah@SARAHs-MBP.fritz.box>
2021-06-17 20:53:06 +0200

    Fix button position using Grid and flexbox

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 document.querySelector('.register-btn').addEventListener('click', function(){
-  let valid = emailIsValid(document.querySelector('.email').value);
 
-  if(valid) {
-    document.querySelectorAll('label')[0].style.display = 'none';
-    document.querySelector('.icon-error').style.display = 'none';
-    document.querySelector('.register').style.border = 'solid 1px hsl(0, 36%, 70%)';
-  }else {
-    document.querySelectorAll('label')[0].style.display = 'inline-block';
+  let email = document.querySelector('.email');
+
+  if(email.validity.typeMismatch || email.validity.valueMissing){
+    document.querySelector('.error-msg').innerHTML = "Please provide a valid email";
     document.querySelector('.icon-error').style.display = 'inline-block';
     document.querySelector('.register').style.border = "2px solid hsl(0, 93%, 68%)";
+  }else {
+    document.querySelector('.error-msg').innerHTML = " ";
+    document.querySelector('.icon-error').style.display = 'none';
+    document.querySelector('.register').style.border = 'solid 1px hsl(0, 36%, 70%)';
   }
+
 });
 
-
-function  emailIsValid(email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
 
 let bird = new Audio('sounds/bird.mp3');
 
